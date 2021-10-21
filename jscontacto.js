23f8368a8822023626a485c26b9e9cbde7a510e8
Trini0895 <80925394+Trini0895@users.noreply.github.com>
2021-10-20 22:19:21 -0300

    Add files via upload

---
+++ jscontacto.js
@@ -0,0 +1,102 @@
+//Validacion del formulario
+
+
+(function(){
+    var formulario = document.getElementsByName('formulario')[0],
+    elementos = formulario.elements,
+    boton = document.getElementById('btn');
+
+    var validarNombre = function(e){
+        if (formulario.nombre.value == 0){
+            alert("Completa el campo Nombre");
+            e.preventDefault();
+        }
+    };
+
+    var validarEmail =function(e){
+        if(formulario.email.value == 0 ){
+            alert("Completa el campo Email")
+            e.preventDefault();
+        }
+    };
+
+    var validarTel = function(e){
+        if(formulario.email.value == 0){
+            alert("Completa el campo telefono")
+            e.preventDefault();
+        }
+    };
+
+    var validar = function(e){
+        validarNombre(e);
+        validarEmail(e);
+        validarTel(e);
+    };
+    formulario.addEventListener("submit", validar);
+    
+}())
+
+
+//TEMPLATE
+
+//HEADER
+
+document.getElementById("header").innerHTML = `<div class="logo">
+<img id="logo" src="Imagenes/Logotipo.jpg">
+</div>
+
+<nav class="nav">
+<ul class="navegacion">
+    <li class="navegation" id="index"><a href="index.html" target="_blank" >Inicio</a></li>
+    <li class="navegation" id="productos"><a href="productos.html" target="_blank">Productos</a></li>
+    <li class="navegation" id="ubicacion"><a href="ubicacion.html" target="_blank">Ubicación</a></li>
+    <li class="navegation" id="contacto-1"><a href="contacto.html" >Contacto</a></li>
+</ul>                
+</nav>
+`
+
+//FOOTER
+
+document.getElementById("footer").innerHTML = `<div class="pie-1">
+
+<div class="sec" id="nav-pie">
+    <p>NAVEGACIÓN</p>
+    <ul>
+        <li><a href="index.html" >Inicio</a></li>
+        <li><a href="productos.html" target="_blank">Productos</a></li>
+        <li><a href="ubicacion.html" target="_blank">Ubicación</a></li>
+        <li><a href="contacto.html" target="_blank">Contacto</a></li>
+    </ul>
+</div>
+
+<div class="sec" id="redes">
+    <p>REDES SOCIALES</p>
+    <ul>
+        <li><i class="fa fa-facebook-f">  www.facebook.com/sugarandcoffee</i></li>
+        <li><i class="fa fa-twitter">  www.twitter.com/sugarandcoffee</i></li>
+        <li><i class="fa fa-instagram">  @sugarandcoffee</i></li>
+    </ul>
+    
+</div>
+
+<div class="sec" id="contacto-2">
+    <p>CONTACTO</p>
+
+    <ul>
+        <li><i class="fa fa-envelope">  sugarandcoffee@gmail.com</i></li>
+        <li><i class="fa fa-phone">  011-34564345</i></li>
+        <li><i class="fa fa-map-marker" aria-hidden="true">  Guemes 1564</i></li>
+
+    </ul>
+</div>
+
+</div>
+
+<div class="pie-2">
+<p>© sugar&coffee derechos de autor</p>
+</div>
+`
+
+
+
+
---
+++ jsindex.js
@@ -0,0 +1,53 @@
+document.getElementById("header").innerHTML = `<div class="logo">
+<img id="logo" src="Imagenes/Logotipo.jpg">
+</div>
+
+<nav class="nav">
+<ul class="navegacion">
+    <li class="navegation" id="index"><a href="index.html" >Inicio</a></li>
+    <li class="navegation" id="productos"><a href="productos.html" target="_blank">Productos</a></li>
+    <li class="navegation" id="ubicacion"><a href="ubicacion.html" target="_blank">Ubicación</a></li>
+    <li class="navegation" id="contacto-1"><a href="contacto.html" target="_blank">Contacto</a></li>
+</ul>                
+</nav>
+`
+
+document.getElementById("footer").innerHTML = `<div class="pie-1">
+
+<div class="sec" id="nav-pie">
+    <p>NAVEGACIÓN</p>
+    <ul>
+        <li><a href="index.html" >Inicio</a></li>
+        <li><a href="productos.html" target="_blank">Productos</a></li>
+        <li><a href="ubicacion.html" target="_blank">Ubicación</a></li>
+        <li><a href="contacto.html" target="_blank">Contacto</a></li>
+    </ul>
+</div>
+
+<div class="sec" id="redes">
+    <p>REDES SOCIALES</p>
+    <ul>
+        <li><i class="fa fa-facebook-f">  www.facebook.com/sugarandcoffee</i></li>
+        <li><i class="fa fa-twitter">  www.twitter.com/sugarandcoffee</i></li>
+        <li><i class="fa fa-instagram">  @sugarandcoffee</i></li>
+    </ul>
+    
+</div>
+
+<div class="sec" id="contacto-2">
+    <p>CONTACTO</p>
+
+    <ul>
+        <li><i class="fa fa-envelope">  sugarandcoffee@gmail.com</i></li>
+        <li><i class="fa fa-phone">  011-34564345</i></li>
+        <li><i class="fa fa-map-marker" aria-hidden="true">  Guemes 1564</i></li>
+
+    </ul>
+</div>
+
+</div>
+
+<div class="pie-2">
+<p>© sugar&coffee derechos de autor</p>
+</div>
+`
